fb1893a2294a549f92cd7828d2d60547dde726d7
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-05-11 11:15:02 -0500

    Add GPU device count in log folder name.

--- tests/throughput/run.py
+++ tests/throughput/run.py
@@ -127,6 +127,13 @@ with open(
 ) as f:
     json.dump(for_insert, f)
 
+if os.environ.get("BOOM_GPU__ENABLED", "false").lower() == "true":
+    n_gpus = len(
+        [d for d in os.environ.get("BOOM_GPU__DEVICE_IDS", "0").split(",") if d.strip()]
+    )
+else:
+    n_gpus = 0
+
 logs_dir = os.path.join(
     f"{args.boom_repo_dir}/logs",
     "boom-"
@@ -134,6 +141,7 @@ logs_dir = os.path.join(
         f"na={args.n_alert_workers}-"
         f"ne={args.n_enrichment_workers}-"
         f"nf={args.n_filter_workers}"
+        f"ng={n_gpus}"
     ),
 )
 
